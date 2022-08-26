aa51a5faf509d4be518333bbb2d55b223a519d31
Ansis Maleckis <maleckisansis@gmail.com>
2022-08-26 18:04:03 +0300

    finished stage 3

--- scraper.py
+++ scraper.py
@@ -3,12 +3,20 @@ from bs4 import BeautifulSoup
 
 def main():
 
+    file = open('source.html', 'wb')
     response = requests.get(input("Input the URL:"), headers={'Accept-Language': 'en-US,en;q=0.5'})
-    try:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        print({"title" : soup.find("h1").contents.pop(), "description" : soup.find("span", {'data-testid': 'plot-xl'}).contents.pop()})
-    except:
-        print("Invalid movie page!")
+    if not response:
+        print("The URL returned {}!".format(response.status_code))
+    else:
+        try:
+            # soup = BeautifulSoup(response.content, 'html.parser')
+            # print({"title" : soup.find("h1").contents.pop(), "description" : soup.find("span", {'data-testid': 'plot-xl'}).contents.pop()})
+            file.write(response.content)
+            file.close()
+            print("Content saved.")
+        except:
+            file.close()
+            print("Error!")
 
 if __name__ == "__main__":
     main()
