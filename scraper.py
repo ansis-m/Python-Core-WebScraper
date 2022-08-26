5f59098f838c7621dbd16ed46bb6b798544ddf52
Ansis Maleckis <maleckisansis@gmail.com>
2022-08-26 21:09:09 +0300

    working on stage 4 getting there

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "https://www.nature.com/nature/articles?sort=PubDate&year=2020&page=3"
+url_base = "https://www.nature.com"
 
 def main():
 
@@ -13,11 +14,25 @@ def main():
         try:
             # print(response.content)
             soup = BeautifulSoup(response.content, 'html.parser')
-            print(soup.find_all("a", {'data-track-action' : 'view article'}))
+            for a in soup.find_all('article'):
+                if a.find('span', {'class':'c-meta__type'}).text == 'News':
+                    print(a.find('a').text)
+                    print(a.find('a').get('href'))
+                    article = requests.get(url_base + a.find('a').get('href'), headers={'Accept-Language': 'en-US,en;q=0.5'})
+                    article_soup = BeautifulSoup(article.content, 'html.parser')
+                    print("\n\n******************************************************")
+                    for p in article_soup.find_all(lambda tag: tag.name == 'p' and not tag.attrs):
+                        print(p.text)
+                    print("\n\n******************************************************")
 
-            for PageElement in soup.find_all("a", {'data-track-action' : 'view article'}):
-                print("*** {} ***".format(PageElement.contents.pop()))
-            print({"title" : soup.find("h1").contents.pop(), "description" : soup.find("span", {'data-testid': 'plot-xl'}).contents.pop()})
+
+
+
+
+        # print(soup.find_all("a", {'data-track-action' : 'view article'}))
+            # for PageElement in soup.find_all("a", {'data-track-action' : 'view article'}):
+            #     print("*** {} ***".format(PageElement.contents.pop()))
+            # print({"title" : soup.find("h1").contents.pop(), "description" : soup.find("span", {'data-testid': 'plot-xl'}).contents.pop()})
             # file.write(response.content)
             # file.close()
             print("Content saved.")
